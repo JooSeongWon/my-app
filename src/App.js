c1f311140e707df1e02619e77f93a00f7bee927a
JooSeongWon <scan0359@naver.com>
2022-10-16 14:41:55 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Sub from './Sub';
 
+//&& : true면 보여주고 false면 안보여줌
 function App() {
+  console.log('App 실행');
+  const [users, setUsers] = useState([]);
+
+  const download = () => {
+    let sample = [
+      { id: 1, name: '홍길동' },
+      { id: 2, name: '임꺽정' },
+      { id: 3, name: '장보고' },
+      { id: 4, name: '코스' },
+    ];
+    setUsers(sample);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id}.{u.name}
+        </h1>
+      ))}
     </div>
   );
 }
